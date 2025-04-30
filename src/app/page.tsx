134ecd287ac1225c0dcaa54d6dfa02d9b4e3e841
Masashi Kawafuji <masashikawafuji@proton.me>
2025-04-30 21:14:08 +0900

    Add Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section className="max-w-7xl px-4 mx-auto">
+        <h1 className="py-4 text-2xl font-bold">useTransition Demo</h1>
+      </section>
+    </main>
+  );
 }
